2d5525a8107e1cb08d9cfdf93527fdcf9e2d7ce6
Adam Cardoso <adamjuniorcardoso@gmail.com>
2024-01-03 18:25:25 -0300

    add: implementando as exceções nos métodos de cadastro das classes CadastroDenuncia e CadastroUsuario

--- trabalho-final-modulo-java/notificaapp/src/views/CadastroUsuario.java
+++ trabalho-final-modulo-java/notificaapp/src/views/CadastroUsuario.java
@@ -1,61 +1,83 @@
 package views;
 
-import entities.Denuncia;
-import entities.Localizacao;
 import entities.Usuario;
-import entities.enums.Categoria;
-import entities.enums.Situacao;
 import entities.enums.TipoUsuario;
+import exceptions.InvalidInputException;
 import interfaces.IUsuarioCadastro;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Random;
 import java.util.Scanner;
 public class CadastroUsuario implements IUsuarioCadastro{
     Scanner scanner = new Scanner(System.in);
+    private static final Random random = new Random();
+
+    @Override
     public Usuario cadastrarUsuario() {
 
         Usuario usuario = new Usuario();
-        int idUsuario = (int) (Math.random() * 1000);
-        usuario.setIdUsuario(idUsuario);
+        try {
+            int idUsuario = random.nextInt(1000);
+            usuario.setIdUsuario(idUsuario);
 
-        System.out.println("Digite o nome do usuário:");
-        String nomeUsuario = scanner.nextLine();
-        usuario.setNomeUsuario(nomeUsuario);
+            System.out.println("Digite o nome do usuário:");
+            String nomeUsuario = scanner.nextLine();
+            if (nomeUsuario.isEmpty()) {
+                throw new InvalidInputException("Nome do usuário não pode ser vazio.");
+            }
 
-        System.out.println("Digite o número do celular:");
-        String numeroCelular = scanner.nextLine();
-        usuario.setNumeroCelular(numeroCelular);
+            usuario.setNomeUsuario(nomeUsuario);
 
-        System.out.println("Digite a senha do usuário:");
-        String senhaUsuario = scanner.nextLine();
-        usuario.setSenhaUsuario(senhaUsuario);
+            System.out.println("Digite o número do celular:");
+            String numeroCelular = scanner.nextLine();
+            if (!numeroCelular.matches("\\d{10}")) {
+                throw new InvalidInputException("Número de celular inválido. Deve conter 10 dígitos numéricos.");
+            }
 
-        System.out.println("Digite a Etnia:");
-        String etnia = scanner.nextLine();
-        usuario.setEtniaUsuario(etnia);
+            usuario.setNumeroCelular(numeroCelular);
 
-        System.out.println("Selecione o TipoUsuario:");
-        for (TipoUsuario tipoUsuario : TipoUsuario.values()) {
-            System.out.println(tipoUsuario.ordinal() + " - " + tipoUsuario);
+            System.out.println("Digite a senha do usuário:");
+            String senhaUsuario = scanner.nextLine();
+            if (senhaUsuario.length() < 6) {
+                throw new InvalidInputException("Senha deve conter pelo menos 6 caracteres.");
+            }
+
+            usuario.setSenhaUsuario(senhaUsuario);
+
+            System.out.println("Digite a Etnia:");
+            String etnia = scanner.nextLine();
+            usuario.setEtniaUsuario(etnia);
+
+            System.out.println("Selecione o TipoUsuario:");
+            for (TipoUsuario tipoUsuario : TipoUsuario.values()) {
+                System.out.println(tipoUsuario.ordinal() + " - " + tipoUsuario);
+            }
+
+            int opcaoTipo = scanner.nextInt();
+            if (opcaoTipo < 0 || opcaoTipo >= TipoUsuario.values().length) {
+                throw new InvalidInputException("Opção de tipo de usuário inválida.");
+            }
+
+            TipoUsuario tipo = TipoUsuario.values()[opcaoTipo];
+            usuario.setTipoUsuario(tipo);
+        } catch (InvalidInputException e) {
+            System.out.println("Tipo de entrada inválida: " + e.getMessage());
         }
-        int opcaoTipo = scanner.nextInt();
-        TipoUsuario tipo = TipoUsuario.values()[opcaoTipo];
-        usuario.setTipoUsuario(tipo);
 
         return usuario;
     }
 
+    @Override
     public Usuario editarUsuario(int idUsuario) {
         //ADICIONAR LÓGICA
         return null;
     }
 
+    @Override
     public void excluirUsuario(Usuario usuarioLogado) {
         //ADICIONAR LÓGICA
     }
 
+    @Override
     public void visualizarUsuario(Usuario usuario) {
         System.out.println("ID do Usuário: " + usuario.getIdUsuario());
         System.out.println("Nome do Usuário: " + usuario.getNomeUsuario());
