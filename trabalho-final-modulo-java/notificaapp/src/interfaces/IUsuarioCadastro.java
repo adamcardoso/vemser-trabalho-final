58372eca3b4452523296551c4843e741abd49a00
Assucena Saldanha Araujo <assucena.araujo@dbccompany.com.br>
2024-01-04 11:03:39 -0300

    adição dos metodos excluir e editar do crud usuario

--- trabalho-final-modulo-java/notificaapp/src/interfaces/IUsuarioCadastro.java
+++ trabalho-final-modulo-java/notificaapp/src/interfaces/IUsuarioCadastro.java
@@ -1,16 +1,18 @@
 package interfaces;
 
+import entities.Denuncia;
 import entities.Usuario;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public interface IUsuarioCadastro {
 
     Usuario cadastrarUsuario();
 
-    Usuario editarUsuario(int idUsuario);
+    Usuario editarUsuario(int idUsuario ,Usuario usuario);
 
-    void excluirUsuario(Usuario IdUsuarioLogado);
+    void excluirUsuario(int idUsuario, HashMap<Integer, Usuario> usuario);
 
     void visualizarUsuario(Usuario usuario);
 }
--- trabalho-final-modulo-java/notificaapp/src/views/CadastroUsuario.java
+++ trabalho-final-modulo-java/notificaapp/src/views/CadastroUsuario.java
@@ -1,10 +1,15 @@
 package views;
 
+import entities.Denuncia;
 import entities.Usuario;
 import entities.enums.TipoUsuario;
 import exceptions.InvalidInputException;
 import interfaces.IUsuarioCadastro;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Random;
 import java.util.Scanner;
 public class CadastroUsuario implements IUsuarioCadastro{
     Scanner scanner = new Scanner(System.in);
@@ -12,7 +17,7 @@ public class CadastroUsuario implements IUsuarioCadastro{
 
     @Override
     public Usuario cadastrarUsuario() {
-        //CRIADO LOGICA DE CADASTRAR - REVISÃO NECESSÁRIA
+
         Usuario usuario = new Usuario();
         try {
             int idUsuario = random.nextInt(1000);
@@ -46,6 +51,28 @@ public class CadastroUsuario implements IUsuarioCadastro{
             String etnia = scanner.nextLine();
             usuario.setEtniaUsuario(etnia);
 
+            System.out.println("Digite sua data de nascimento (dd/MM/yyyy): ");
+            String data = scanner.nextLine();
+
+            try {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                dateFormat.setLenient(false);
+
+                Date dataNascimento = dateFormat.parse(data);
+                usuario.setDataNascimento(dataNascimento);
+            } catch (Exception e) {
+                System.out.println("Formato de data inválido. Certifique-se de usar o formato dd/MM/yyyy.");
+            }
+
+            System.out.println("Digite sua faixa salárial:");
+            String classesocial = scanner.nextLine();
+            usuario.setEtniaUsuario( classesocial);
+
+            System.out.println("Digite o Genero:");
+            String genero = scanner.nextLine();
+            usuario.setEtniaUsuario(genero);
+
+
             System.out.println("Selecione o TipoUsuario:");
             for (TipoUsuario tipoUsuario : TipoUsuario.values()) {
                 System.out.println(tipoUsuario.ordinal() + " - " + tipoUsuario);
@@ -66,14 +93,89 @@ public class CadastroUsuario implements IUsuarioCadastro{
     }
 
     @Override
-    public Usuario editarUsuario(int idUsuario) {
-        //ADICIONAR LÓGICA
-        return null;
+    public Usuario editarUsuario(int idUsuario, Usuario usuario) {
+        try {
+            usuario.setIdUsuario(idUsuario);
+            System.out.println("Digite o nome do usuário:");
+
+            String nomeUsuario = scanner.nextLine();
+
+            if (nomeUsuario.isEmpty()) {
+                throw new InvalidInputException("Nome do usuário não pode ser vazio.");
+            }
+
+            usuario.setNomeUsuario(nomeUsuario);
+
+            System.out.println("Digite o número do celular:");
+            String numeroCelular = scanner.nextLine();
+            if (!numeroCelular.matches("\\d{10}")) {
+                throw new InvalidInputException("Número de celular inválido. Deve conter 10 dígitos numéricos.");
+            }
+
+            usuario.setNumeroCelular(numeroCelular);
+
+            System.out.println("Digite a senha do usuário:");
+            String senhaUsuario = scanner.nextLine();
+            if (senhaUsuario.length() < 6) {
+                throw new InvalidInputException("Senha deve conter pelo menos 6 caracteres.");
+            }
+
+            usuario.setSenhaUsuario(senhaUsuario);
+
+            System.out.println("Digite a Etnia:");
+            String etnia = scanner.nextLine();
+            usuario.setEtniaUsuario(etnia);
+
+            System.out.println("Digite sua data de nascimento (dd/MM/yyyy): ");
+            String data = scanner.nextLine();
+
+            try {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                dateFormat.setLenient(false);
+
+                Date dataNascimento = dateFormat.parse(data);
+                usuario.setDataNascimento(dataNascimento);
+            } catch (Exception e) {
+                System.out.println("Formato de data inválido. Certifique-se de usar o formato dd/MM/yyyy.");
+            }
+
+            System.out.println("Digite sua faixa salárial:");
+            String classesocial = scanner.nextLine();
+            usuario.setEtniaUsuario(classesocial);
+
+            System.out.println("Digite o Genero:");
+            String genero = scanner.nextLine();
+            usuario.setEtniaUsuario(genero);
+
+            System.out.println("Selecione o TipoUsuario:");
+            for (TipoUsuario tipoUsuario : TipoUsuario.values()) {
+                System.out.println(tipoUsuario.ordinal() + " - " + tipoUsuario);
+            }
+
+            int opcaoTipo = scanner.nextInt();
+            if (opcaoTipo < 0 || opcaoTipo >= TipoUsuario.values().length) {
+                throw new InvalidInputException("Opção de tipo de usuário inválida.");
+            }
+
+            TipoUsuario tipo = TipoUsuario.values()[opcaoTipo];
+            usuario.setTipoUsuario(tipo);
+        } catch (InvalidInputException e) {
+            System.out.println("Tipo de entrada inválida: " + e.getMessage());
+        }
+
+        return usuario;
     }
 
+
+
     @Override
-    public void excluirUsuario(Usuario usuarioLogado) {
-        //ADICIONAR LÓGICA
+    public void excluirUsuario(int idUsuario, HashMap<Integer, Usuario> usuario) {
+
+
+        if (usuario.containsKey(idUsuario)){
+            usuario.remove(idUsuario);
+        } else
+            System.out.println("Usuario não encontrado");
     }
 
     @Override
@@ -83,10 +185,12 @@ public class CadastroUsuario implements IUsuarioCadastro{
         System.out.println("Número do Celular: " + usuario.getNumeroCelular());
         System.out.println("Senha do Usuário: " + usuario.getSenhaUsuario());
         System.out.println("Etnia do Usuário: " + usuario.getEtniaUsuario());
-        System.out.println("Tipo do Usuário: " + usuario.getTipoUsuario());
-        System.out.println("Genero: " + usuario.getGeneroUsuario());
         System.out.println("Data Nascimento: " + usuario.getDataNascimento());
         System.out.println("Classe Social: " + usuario.getClasseSocial());
+        System.out.println("Genero: " + usuario.getGeneroUsuario());
+        System.out.println("Tipo do Usuário: " + usuario.getTipoUsuario());
+
+
     }
 
 }
