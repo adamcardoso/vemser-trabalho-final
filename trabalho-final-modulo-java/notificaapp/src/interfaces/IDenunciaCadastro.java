bad1498f2f7f6959d4bf6f88dd94560318778a97
Anderson Luiz <trkz.anderson@gmail.com>
2024-01-04 07:32:52 -0300

    feat: atualiza método de interface excluirDenuncia

--- trabalho-final-modulo-java/notificaapp/src/interfaces/IDenunciaCadastro.java
+++ trabalho-final-modulo-java/notificaapp/src/interfaces/IDenunciaCadastro.java
@@ -2,14 +2,14 @@ package interfaces;
 
 import entities.Denuncia;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public interface IDenunciaCadastro {
     Denuncia cadastrarDenuncia();
 
     Denuncia editarDenuncia(int idDenuncia, Denuncia denuncia);
 
-    int excluirDenuncia(ArrayList<Denuncia> denuncia);
+    void excluirDenuncia(int idDenuncia, HashMap<Integer, Denuncia> denuncias);
 
     void visualizarDenuncia(Denuncia denuncia);
 }
