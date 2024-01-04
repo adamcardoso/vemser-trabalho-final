5f9aac29349b14e1648d838d29930ffb12878f8e
Anderson Luiz <trkz.anderson@gmail.com>
2024-01-04 07:32:01 -0300

    feat: atualiza método de interface editarDenuncia

--- trabalho-final-modulo-java/notificaapp/src/interfaces/IDenunciaCadastro.java
+++ trabalho-final-modulo-java/notificaapp/src/interfaces/IDenunciaCadastro.java
@@ -5,10 +5,9 @@ import entities.Denuncia;
 import java.util.ArrayList;
 
 public interface IDenunciaCadastro {
-
     Denuncia cadastrarDenuncia();
 
-    Denuncia editarDenuncia(int idDenuncia);
+    Denuncia editarDenuncia(int idDenuncia, Denuncia denuncia);
 
     int excluirDenuncia(ArrayList<Denuncia> denuncia);
 
