08e82fbc1a8983345e9658312121188625d5472b
Renata Oliveira Schäfer <renataunisj@gmail.com>
2024-01-05 13:11:58 -0300

    Arrumando Lógica de Cadastro Denuncia

--- trabalho-final-modulo-java/notificaapp/src/views/CadastroDenuncia.java
+++ trabalho-final-modulo-java/notificaapp/src/views/CadastroDenuncia.java
@@ -16,96 +16,46 @@ public class CadastroDenuncia implements IDenunciaCadastro {
     Scanner scanner = new Scanner(System.in);
     @Override
     public Denuncia cadastrarDenuncia() {
-        Denuncia denuncia = new Denuncia();
 
-        try {
-            int idDenuncia = (int) (Math.random() * 1000);
+        int idDenuncia = (int) (Math.random() * 1000);
+        System.out.println("Digite o nome do usuário:");
+        String nomeUsuario = scanner.nextLine();
 
-            denuncia.setIdDenuncia(idDenuncia);
-
-            System.out.println("Digite o nome do usuário:");
-
-            String nomeUsuario = scanner.nextLine();
-
-            if (nomeUsuario.isEmpty()) {
-                throw new InvalidInputException("Nome do usuário não pode ser vazio.");
-            }
-
-            Usuario usuario = new Usuario();
-
-            usuario.setNomeUsuario(nomeUsuario);
-
-            denuncia.setUsuario(usuario);
-
-            System.out.println("Digite a Descrição:");
-
-            String descricao = scanner.nextLine();
-
-            denuncia.setDescricao(descricao);
-
-            Localizacao localizacao = new Localizacao();
-
-            try {
-                System.out.println("Digite a Localização (Longitude):");
-
-                double longitude = scanner.nextDouble();
-
-                localizacao.setLongitude(longitude);
-            } catch (Exception e) {
-                throw new InvalidInputException("Valor deve ser numérico.");
-            }
-
-            try {
-                System.out.println("Digite a Localização (Latitude):");
-
-                double latitude = scanner.nextDouble();
-
-                localizacao.setLatitute(latitude);
-            } catch (Exception e) {
-                throw new InvalidInputException("Valor deve ser numérico.");
-            }
-
-            denuncia.setLocal(localizacao);
-
-            System.out.println("Selecione o Status da Denúncia:");
-
-            for (Situacao situacao : Situacao.values()) {
-                System.out.println(situacao.ordinal() + " - " + situacao);
-            }
-
-            try {
-                int opcaoStatus = scanner.nextInt();
-
-                Situacao statusDenuncia = Situacao.values()[opcaoStatus];
-
-                denuncia.setStatusDenuncia(statusDenuncia);
-            } catch (Exception e) {
-                throw new InvalidInputException("Valor inválido.");
-            }
-
-            System.out.println("Selecione a Categoria da Denúncia:");
-
-            for (Categoria categoria : Categoria.values()) {
-                System.out.println(categoria.ordinal() + " - " + categoria);
-            }
-
-            try {
-                int opcaoCategoria = scanner.nextInt();
-
-                Categoria categoriaSelecionada = Categoria.values()[opcaoCategoria];
-
-                denuncia.setCategoria(categoriaSelecionada);
-            } catch (Exception e) {
-                throw new InvalidInputException("Valor inválido.");
-            }
-
-            LocalDateTime dataHora = LocalDateTime.now();
-
-            denuncia.setDataHora(dataHora);
-        } catch (InvalidInputException e) {
-            System.out.println("Tipo de entrada inválida: " + e.getMessage());
-            return null;
+        if (nomeUsuario.isEmpty()) {
+            throw new InvalidInputException("Nome do usuário não pode ser vazio.");
         }
+
+        Usuario usuario = new Usuario(nomeUsuario);
+
+        System.out.println("Digite a Descrição:");
+        String descricao = scanner.nextLine();
+
+        System.out.println("Digite a Localização (Longitude):");
+        double longitude = scanner.nextDouble();
+
+        System.out.println("Digite a Localização (Latitude):");
+        double latitude = scanner.nextDouble();
+        Localizacao localizacao = new Localizacao(latitude, longitude);
+
+        System.out.println("Selecione o Status da Denúncia:");
+        for (Situacao situacao : Situacao.values()) {
+            System.out.println(situacao.ordinal() + " - " + situacao);
+        }
+
+        int opcaoStatus = scanner.nextInt();
+        Situacao statusDenuncia = Situacao.values()[opcaoStatus];
+
+        System.out.println("Selecione a Categoria da Denúncia:");
+        for (Categoria categoria : Categoria.values()) {
+            System.out.println(categoria.ordinal() + " - " + categoria);
+        }
+
+        int opcaoCategoria = scanner.nextInt();
+        Categoria categoriaSelecionada = Categoria.values()[opcaoCategoria];
+
+        LocalDateTime dataHora = LocalDateTime.now();
+
+        Denuncia denuncia = new Denuncia(idDenuncia, descricao, localizacao, usuario, dataHora, statusDenuncia, categoriaSelecionada);
 
         return denuncia;
     }
@@ -114,9 +64,7 @@ public class CadastroDenuncia implements IDenunciaCadastro {
     public Denuncia editarDenuncia(int idDenuncia, Denuncia denuncia) {
         try {
             denuncia.setIdDenuncia(idDenuncia);
-
             System.out.println("Digite o nome do usuário:");
-
             String nomeUsuario = scanner.nextLine();
 
             if (nomeUsuario.isEmpty()) {
@@ -124,24 +72,16 @@ public class CadastroDenuncia implements IDenunciaCadastro {
             }
 
             Usuario usuario = new Usuario();
-
             usuario.setNomeUsuario(nomeUsuario);
-
             denuncia.setUsuario(usuario);
-
             System.out.println("Digite a Descrição:");
-
             String descricao = scanner.nextLine();
-
             denuncia.setDescricao(descricao);
-
             Localizacao localizacao = new Localizacao();
 
             try {
                 System.out.println("Digite a Localização (Longitude):");
-
                 double longitude = scanner.nextDouble();
-
                 localizacao.setLongitude(longitude);
             } catch (Exception e) {
                 throw new InvalidInputException("Valor deve ser numérico.");
@@ -149,43 +89,33 @@ public class CadastroDenuncia implements IDenunciaCadastro {
 
             try {
                 System.out.println("Digite a Localização (Latitude):");
-
                 double latitude = scanner.nextDouble();
-
                 localizacao.setLatitute(latitude);
             } catch (Exception e) {
                 throw new InvalidInputException("Valor deve ser numérico.");
             }
-
             denuncia.setLocal(localizacao);
-
             System.out.println("Selecione o Status da Denúncia:");
-
             for (Situacao situacao : Situacao.values()) {
                 System.out.println(situacao.ordinal() + " - " + situacao);
             }
 
             try {
                 int opcaoStatus = scanner.nextInt();
-
                 Situacao statusDenuncia = Situacao.values()[opcaoStatus];
-
                 denuncia.setStatusDenuncia(statusDenuncia);
             } catch (Exception e) {
                 throw new InvalidInputException("Valor inválido.");
             }
 
             System.out.println("Selecione a Categoria da Denúncia:");
-
             for (Categoria categoria : Categoria.values()) {
                 System.out.println(categoria.ordinal() + " - " + categoria);
             }
 
             try {
                 int opcaoCategoria = scanner.nextInt();
-
                 Categoria categoriaSelecionada = Categoria.values()[opcaoCategoria];
-
                 denuncia.setCategoria(categoriaSelecionada);
             } catch (Exception e) {
                 throw new InvalidInputException("Valor inválido.");
@@ -209,22 +139,14 @@ public class CadastroDenuncia implements IDenunciaCadastro {
 
     @Override
     public void visualizarDenuncia(Denuncia denuncia) {
-            System.out.println("ID da Denúncia: " + denuncia.getIdDenuncia());
-
-            System.out.println("Nome do Usuário: " + denuncia.getUsuario().getNomeUsuario());
-
-            System.out.println("Descrição: " + denuncia.getDescricao());
-
-            System.out.println("Localização (Longitude): " + denuncia.getLocal().getLongitude());
-
-            System.out.println("Localização (Latitude): " + denuncia.getLocal().getLatitute());
-
-            System.out.println("Status da Denúncia: " + denuncia.getStatusDenuncia());
-
-            System.out.println("Categoria: " + denuncia.getCategoria());
-
-            System.out.println("Data e Hora: " + denuncia.getDataHora());
-
-            System.out.println("--------------------------------------");
+        System.out.println("ID da Denúncia: " + denuncia.getIdDenuncia());
+        System.out.println("Nome do Usuário: " + denuncia.getUsuario().getNomeUsuario());
+        System.out.println("Descrição: " + denuncia.getDescricao());
+        System.out.println("Localização (Longitude): " + denuncia.getLocal().getLongitude());
+        System.out.println("Localização (Latitude): " + denuncia.getLocal().getLatitute());
+        System.out.println("Status da Denúncia: " + denuncia.getStatusDenuncia());
+        System.out.println("Categoria: " + denuncia.getCategoria());
+        System.out.println("Data e Hora: " + denuncia.getDataHora());
+        System.out.println("--------------------------------------");
     }
 }
