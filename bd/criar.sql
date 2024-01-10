3030029a22aa7535b91946697cb13a1e428cf445
Adam Cardoso <adamjuniorcardoso@gmail.com>
2024-01-10 13:23:27 -0300

    fix: corrigindo o nome dos atributos para snake case

--- bd/criar.sql
+++ bd/criar.sql
@@ -3,25 +3,31 @@ CREATE TABLE NOTIFICA.USUARIO (
     nome_usuario VARCHAR2(255) NOT NULL,
     numero_celular VARCHAR2(20) NOT NULL,
     senha_usuario VARCHAR2(255) NOT NULL,
-    etnia_usuario VARCHAR2(50),
+    id_etnia NUMBER,
     data_nascimento DATE NOT NULL,
-    classe_social VARCHAR2(50),
-    genero_usuario VARCHAR2(20),
-    tipo_usuario VARCHAR2(50) NOT NULL
+    id_classe_social NUMBER,
+    id_genero NUMBER NOT NULL,
+    id_tipo_usuario NUMBER NOT NULL,
+    FOREIGN KEY (id_etnia) REFERENCES NOTIFICA.ETNIA(id_etnia),
+    FOREIGN KEY (id_classe_social) REFERENCES NOTIFICA.CLASSE_SOCIAL(id_classe_social),
+    FOREIGN KEY (id_genero) REFERENCES NOTIFICA.GENERO(id_genero),
+    FOREIGN KEY (id_tipo_usuario) REFERENCES NOTIFICA.TIPO_USUARIO(id_tipo_usuario)
 );
 
 CREATE TABLE NOTIFICA.DENUNCIA (
     id_denuncia NUMBER PRIMARY KEY NOT NULL,
-    descricao VARCHAR2(1000) NOT NULL,
-    latitude NUMBER NOT NULL,
-    longitude NUMBER NOT NULL,
-    usuario_id NUMBER NOT NULL,
-    data_hora TIMESTAMP NOT NULL,
-    status_denuncia VARCHAR2(20) NOT NULL,
-    categoria VARCHAR2(20) NOT NULL,
+    descricao VARCHAR2(1000),
+    latitude NUMBER,
+    longitude NUMBER,
+    id_usuario NUMBER,
+    dataHora TIMESTAMP,
+    id_situacao NUMBER,
+    id_categoria NUMBER,
     curtidas NUMBER,
     validar_denuncia NUMBER,
-    FOREIGN KEY (usuario_id) REFERENCES NOTIFICA.USUARIO(id_usuario)
+    FOREIGN KEY (id_usuario) REFERENCES NOTIFICA.USUARIO(id_usuario),
+    FOREIGN KEY (id_situacao ) REFERENCES NOTIFICA.SITUACAO(id_situacao),
+    FOREIGN KEY (id_categoria) REFERENCES NOTIFICA.CATEGORIA(id_categoria)
 );
 
 CREATE SEQUENCE NOTIFICA.SEQ_USUARIO
