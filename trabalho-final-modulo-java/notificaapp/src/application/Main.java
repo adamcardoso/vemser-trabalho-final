9471def0d6becd5c79f3713e9721196e9eed6da4
NivekSotam <kevin.matoss.km@gmail.com>
2024-01-14 19:38:34 -0300

    fix: main

--- trabalho-final-modulo-java/notificaapp/src/application/Main.java
+++ trabalho-final-modulo-java/notificaapp/src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 import exceptions.DataBaseException;
 import views.Home;
+
 public class Main {
     public static void main(String[] args) throws DataBaseException {
         Home home = new Home();
