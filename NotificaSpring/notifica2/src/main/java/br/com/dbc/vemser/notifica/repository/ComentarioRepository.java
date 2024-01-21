2781d288acfda7c73423e50a542ae423f99b2d5c
André Miranda <andrelmmiranda@gmail.com>
2024-01-21 01:34:13 -0300

    fix: removendo trecho de comentario

--- NotificaSpring/notifica2/src/main/java/br/com/dbc/vemser/notifica/repository/ComentarioRepository.java
+++ NotificaSpring/notifica2/src/main/java/br/com/dbc/vemser/notifica/repository/ComentarioRepository.java
@@ -31,15 +31,10 @@ public class ComentarioRepository {
             stmt.setInt(1, id);
             ResultSet resultSet = stmt.executeQuery();
 
-            if(resultSet.next()){
-                int idComentario = resultSet.getInt("ID_COMENTARIO");
-                String comentario = resultSet.getString("COMENTARIO");
-                int curtida = resultSet.getInt("CURTIDA");
-                int idDenuncia = resultSet.getInt("ID_DENUNCIA");
-                int idUsuario = resultSet.getInt("ID_USUARIO");
+            if(resultSet.next())
+                return Optional.of(new Comentario(resultSet.getInt("ID_COMENTARIO"), resultSet.getString("COMENTARIO"),
+                        resultSet.getInt("CURTIDA"), resultSet.getInt("ID_DENUNCIA"), resultSet.getInt("ID_USUARIO")));
 
-                return Optional.of(new Comentario(idComentario, comentario, curtida, idDenuncia, idUsuario));
-            }
             return Optional.empty();
         } catch (Exception e) {
             throw new Exception(e);
@@ -157,9 +152,9 @@ public class ComentarioRepository {
 
             int res = stmt.executeUpdate();
 
-            if (res > 0){
+            if (res > 0)
                 return this.obterComentarioById(id);
-            } else
+            else
                 throw new Exception("Falha ao atualizar comentário.");
 
         } catch (Exception e){
@@ -211,81 +206,14 @@ public class ComentarioRepository {
         }
     }
 
-
-//    @Override
-//    public boolean removerUsuario(Integer idUsuario) throws DataBaseException {
-//        Connection con = null;
-//
-//        try {
-//            con = ConexaoBancoDeDados.getConnection();
-//            con.setAutoCommit(false);  // Desativa o modo de confirmação automática
-//
-//
-//            // Exclui registros dependentes em COMENTARIO
-//            String sqlDeleteComentario = "DELETE FROM COMENTARIO WHERE id_usuario = ?";
-//            try (PreparedStatement stmtComentario = con.prepareStatement(sqlDeleteComentario)) {
-//                stmtComentario.setInt(1, idUsuario);
-//                stmtComentario.executeUpdate();
-//
-//            }
-//
-//            // Exclui registros dependentes em LOCALIZACAO
-//            String sqlDeleteLocalizacao = "DELETE FROM LOCALIZACAO WHERE id_usuario = ?";
-//            try (PreparedStatement stmtLocalizacao = con.prepareStatement(sqlDeleteLocalizacao)) {
-//                stmtLocalizacao.setInt(1, idUsuario);
-//                stmtLocalizacao.executeUpdate();
-//            }
-//
-//            // Exclui o registro principal em DENUNCIA
-//            String sqlDeleteDenuncia = "DELETE FROM DENUNCIA WHERE id_usuario = ?";
-//            try (PreparedStatement stmtDenuncia = con.prepareStatement(sqlDeleteDenuncia)) {
-//                stmtDenuncia.setInt(1, idUsuario);
-//                stmtDenuncia.executeUpdate();
-//            }
-//
-//            String sqlDeleteUsuario = "DELETE FROM USUARIO WHERE id_usuario = ?";
-//            try (PreparedStatement stmtDenuncia = con.prepareStatement(sqlDeleteUsuario)) {
-//                stmtDenuncia.setInt(1, idUsuario);
-//                int res = stmtDenuncia.executeUpdate();
-//
-//                // Confirma as alterações no banco de dados
-//                con.commit();
-//
-//                return res > 0;
-//            }
-//        } catch (SQLException e) {
-//            // Em caso de erro, faz rollback das alterações no banco de dados
-//            if (con != null) {
-//                try {
-//                    con.rollback();
-//                } catch (SQLException ex) {
-//                    ex.printStackTrace();
-//                }
-//            }
-//            System.err.println("Erro ao remover Usuário!");
-//            throw new DataBaseException("Erro: " + e);
-//        } finally {
-//            try {
-//                if (con != null) {
-//                    con.close();
-//                }
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-
-
-
     public Integer getProximoIdDoUsuario(Connection connection) throws SQLException {
         String sql = "SELECT SEQ_USUARIO.NEXTVAL mysequence from DUAL";
 
         Statement stmt = connection.createStatement();
         ResultSet res = stmt.executeQuery(sql);
 
-        if (res.next()) {
+        if (res.next())
             return res.getInt("mysequence");
-        }
 
         return null;
     }
