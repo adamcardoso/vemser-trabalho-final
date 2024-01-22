911a5bf131957a5b88f8679959725e9cbb225280
Anderson Luiz <trkz.anderson@gmail.com>
2024-01-22 13:41:37 -0300

    feat: adiciona fluxo de usuario com spring web

--- NotificaSpring/notifica2/src/main/java/br/com/dbc/vemser/notifica/dto/usuario/UsuarioDto.java
+++ NotificaSpring/notifica2/src/main/java/br/com/dbc/vemser/notifica/dto/usuario/UsuarioDto.java
@@ -1,4 +1,4 @@
-package br.com.dbc.vemser.notifica.entity;
+package br.com.dbc.vemser.notifica.dto.usuario;
 
 import br.com.dbc.vemser.notifica.entity.enums.ClasseSocial;
 import br.com.dbc.vemser.notifica.entity.enums.Etnia;
@@ -6,22 +6,22 @@ import br.com.dbc.vemser.notifica.entity.enums.Genero;
 import br.com.dbc.vemser.notifica.entity.enums.TipoUsuario;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.time.LocalDate;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Data
-public class Usuario {
+public class UsuarioDto {
     private int idUsuario;
     private String nomeUsuario;
     private String numeroCelular;
+
+    @ToString.Exclude
     private String senhaUsuario;
     private Etnia etniaUsuario;
     private LocalDate dataNascimento;
     private ClasseSocial classeSocial;
     private Genero generoUsuario;
     private TipoUsuario tipoUsuario;
-    private boolean isAdmin;
 }
