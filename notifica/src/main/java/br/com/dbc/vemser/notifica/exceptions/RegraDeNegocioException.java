9ab36e749181da6ee81aec17f86f3c5234bee828
Renata Oliveira Schäfer <renataunisj@gmail.com>
2024-01-25 17:30:19 -0300

    hml

--- notifica/src/main/java/br/com/dbc/vemser/notifica/exceptions/RegraDeNegocioException.java
+++
@@ -1,8 +0,0 @@
-package br.com.dbc.vemser.notifica.exceptions;
-
-public class RegraDeNegocioException extends Exception {
-
-    public RegraDeNegocioException(String message) {
-        super(message);
-    }
-}
