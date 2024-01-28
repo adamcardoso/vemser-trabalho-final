a3bb1634c9f0b4e2f51f173b21ee6d4232a8113d
Renata Oliveira Schäfer <renataunisj@gmail.com>
2024-01-28 20:58:51 -0300

    add: config cors para autorizar tudo

--- src/main/java/br/com/dbc/vemser/notifica/config/OpenApiConfig.java
+++ src/main/java/br/com/dbc/vemser/notifica/config/OpenApiConfig.java
@@ -6,6 +6,8 @@ import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class OpenApiConfig {
@@ -14,10 +16,25 @@ public class OpenApiConfig {
     public OpenAPI springShopOpenAPI() {
         String securitySchemeName = "bearerAuth";
         return new OpenAPI()
-                .info(new Info().title("Pessoa API")
-                        .description("Pessoa API documentação")
+                .info(new Info().title("NOTIFICA API")
+                        .description("Notifica API documentação")
                         .version("v1.0.0")
                         .license(new License().name("Apache 2.0").url("http://springdoc.org")))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName));
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(false)
+                        .maxAge(3600);
+            }
+        };
+    }
 }
+
