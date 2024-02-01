52de254f8847401805e68a04671a92c1b8734d29
Renata Oliveira Schäfer <renataunisj@gmail.com>
2024-02-01 16:58:07 -0300

    fix: regra de negocio e reutilização de metodos

--- src/main/java/br/com/dbc/vemser/notifica/repository/LoginRepository.java
+++ src/main/java/br/com/dbc/vemser/notifica/repository/LoginRepository.java
@@ -1,22 +1,9 @@
 package br.com.dbc.vemser.notifica.repository;
 
-import br.com.dbc.vemser.notifica.config.ConexaoBancoDeDados;
-import br.com.dbc.vemser.notifica.dto.usuario.UsuarioLoginDTO;
 import br.com.dbc.vemser.notifica.entity.Usuario;
-import br.com.dbc.vemser.notifica.entity.enums.ClasseSocial;
-import br.com.dbc.vemser.notifica.entity.enums.Etnia;
-import br.com.dbc.vemser.notifica.entity.enums.Genero;
-import br.com.dbc.vemser.notifica.entity.enums.TipoUsuario;
-import br.com.dbc.vemser.notifica.exceptions.RegraDeNegocioException;
-import lombok.AllArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.Optional;
 
 @Repository
