4d1f258d8c66a70144c34927d2819d84f2a66bf5
Renata Oliveira Schäfer <renataunisj@gmail.com>
2024-02-02 20:39:28 -0300

    add: paginação na listagem de usuarios

--- src/main/java/br/com/dbc/vemser/notifica/repository/AdminRepository.java
+++ src/main/java/br/com/dbc/vemser/notifica/repository/AdminRepository.java
@@ -1,6 +1,8 @@
 package br.com.dbc.vemser.notifica.repository;
 
 import br.com.dbc.vemser.notifica.entity.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,5 +12,6 @@ import java.util.List;
 @Repository
 public interface AdminRepository extends JpaRepository<Usuario, Integer> {
     @Query(value = "SELECT u FROM USUARIO u WHERE u.tipoUsuario = 0")
-    public List<Usuario> usuariosAdmin();
+    List<Usuario> usuariosAdmin();
+
 }
