59600f260d1b7cb48771227ea4561d3518b087e6
André Miranda <andrelmmiranda@gmail.com>
2024-02-03 00:33:52 -0300

    feat: adicionando funcionalidade para curtir e descurtir uma denuncia

--- src/main/java/br/com/dbc/vemser/notifica/controller/documentacao/ICurtidaController.java
+++ src/main/java/br/com/dbc/vemser/notifica/controller/documentacao/ICurtidaController.java
@@ -12,10 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public interface ICurtidaController {
-    @Operation(summary = "Adicionar curtida", description = "Método adiciona e remove curtida de comentário.")
+    @Operation(summary = "Adicionar e remover curtida", description = "Método adiciona e remove curtida de comentário.")
     @ApiResponses(
             value = {
-                    @ApiResponse(responseCode = "200", description = "Retorna void",
+                    @ApiResponse(responseCode = "200", description = "Retorna vazio",
                             content = @Content(mediaType = "application/json", schema = @Schema(implementation = CurtidaDto.class))),
                     @ApiResponse(responseCode = "400", description = "Usuário não encontrado!.",
                             content = @Content(mediaType = "application/json", schema = @Schema(hidden = false, implementation = ErrorResponse.class))),
@@ -24,5 +24,19 @@ public interface ICurtidaController {
             }
     )
     @PostMapping
-    void apoiar(@RequestBody CurtidaDto curtidaDto) throws Exception;
+    void apoiarComentario(@RequestBody CurtidaDto curtidaDto) throws Exception;
+
+    @Operation(summary = "Adicionar e remover curtida", description = "Método adiciona e remove curtida de denúncia.")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Retorna vazio",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CurtidaDto.class))),
+                    @ApiResponse(responseCode = "400", description = "Usuário não encontrado!.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(hidden = false, implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "400", description = "Denúncia não encontrada!",
+                            content = @Content(mediaType = "application/json", schema = @Schema(hidden = false, implementation = ErrorResponse.class)))
+            }
+    )
+    @PostMapping
+    void apoiarDenuncia(@RequestBody CurtidaDto curtidaDto) throws Exception;
 }
