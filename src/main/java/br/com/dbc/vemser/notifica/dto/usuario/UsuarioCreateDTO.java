a1b83d2753e3e94065b318f6f057efdb50e5fece
Renata Oliveira Schäfer <renataunisj@gmail.com>
2024-02-18 15:22:33 -0300

    add: Avisos Test 68% line sem testar envio de email

--- src/main/java/br/com/dbc/vemser/notifica/dto/usuario/UsuarioCreateDTO.java
+++ src/main/java/br/com/dbc/vemser/notifica/dto/usuario/UsuarioCreateDTO.java
@@ -9,11 +9,13 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class UsuarioCreateDTO {
 
---
+++ src/test/java/br/com/dbc/vemser/notifica/service/LoginServiceTest.java
@@ -0,0 +1,12 @@
+package br.com.dbc.vemser.notifica.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+@DisplayName("LoginService - Test")
+class LoginServiceTest {
+
+}
